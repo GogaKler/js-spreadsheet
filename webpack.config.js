0b2d9934daf265eec7ebeebc67f66fc5535d74dc
Egor Kolesnikov <gogakler@gmail.com>
2022-05-13 21:52:43 +0300

    Finish styles feature

--- webpack.config.js
+++ webpack.config.js
@@ -70,13 +70,6 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true,
-            },
-          },
           MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
